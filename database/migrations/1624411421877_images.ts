c609b9872fb696adab9e3fcd48c85eba1a323f82
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2021-07-12 17:07:29 -0300

    Change modals

--- database/migrations/1624411421877_images.ts
+++ database/migrations/1624411421877_images.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class Files extends BaseSchema {
-  protected tableName = 'files'
+export default class Images extends BaseSchema {
+  protected tableName = 'images'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
